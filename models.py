72b01aa400cc627a450e1f9dac8978db4beb1dad
Eric Templeton <erictempleton1@gmail.com>
2015-06-12 07:51:45 -0400

    added user model

--- models.py
+++ models.py
@@ -1,3 +1,11 @@
 import datetime
 from flask import url_for
 from aggtron import db
+
+
+class User(db.Document):
+    email = db.EmailField(unique=True)
+    password = db.StringField(default=True)
+    active = db.BooleanField(default=True)
+    isAdmin = db.BooleanField(default=False)
+    timestamp = db.DateTimeField(default=datetime.datetime.now())
