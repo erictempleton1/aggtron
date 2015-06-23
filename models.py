1a616da45d3e13bd1c4984584098a5739b980b4f
Eric Templeton <erictempleton1@gmail.com>
2015-06-23 15:22:34 -0400

    removing mongo for sqlite

--- models.py
+++ models.py
@@ -3,22 +3,14 @@ from flask import url_for
 from aggtron import db
 
 
-class User(db.Document):
-    email = db.EmailField(unique=True)
-    password = db.StringField(default=True)
-    active = db.BooleanField(default=True)
-    isAdmin = db.BooleanField(default=False)
-    timestamp = db.DateTimeField(default=datetime.datetime.now())
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
 
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
 
-class AuthPass(db.EmbeddedDocument):
-    username = db.StringField(default=True)
-    password = db.StringField(default=True)
-
-
-class Project(db.Document):
-    name = db.StringField(default=True)
-    database = db.StringField(default=True)
-    created = db.DateTimeField(default=datetime.datetime.now())
-    created_by = db.ReferenceField(User)
-    #auth = db.EmbeddedDocumentField(AuthPass, default=AuthPass)
+    def __repr__(self):
+        return '<Email: {0}>'.format(self.email)
