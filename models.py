151aac64ec8286a10f11616fcfd4211fb923ccf2
Eric Templeton <erictempleton1@gmail.com>
2015-06-25 16:32:41 -0500

    minor spacing change

--- models.py
+++ models.py
@@ -5,11 +5,13 @@ from aggtron import db
 
 class Users(db.Model):
 
+
     __table_args__ = {'extend_existing': True}
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
+
 
     def __init__(self, email, password):
         self.email = email
