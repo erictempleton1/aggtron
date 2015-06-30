a2570b364f3b1753af81331863db598fcd5febb5
Eric Templeton <erictempleton1@gmail.com>
2015-06-30 08:10:32 -0400

    adding simple tests

--- tests/connect_tests.py
+++ tests/connect_tests.py
@@ -3,22 +3,42 @@ top_dir = os.path.join(os.path.dirname(__file__), '../../')
 sys.path.append(top_dir)
 
 import aggtron
+from aggtron import db
+from flask import Flask
 import unittest
 import tempfile
+from aggtron.models import Users, Project
+from flask.ext.login import login_user, current_user
+from flask.ext.testing import TestCase
 
 
-class ConnectTestCase(unittest.TestCase):
+class ConnectTestCase(TestCase):
 
+
+    render_templates = False
+
+    def create_app(self):
+        app = Flask(__name__)
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/erictempleton/Documents/Projects/myenv/aggtron/test.db'
+        app.config['TESTING'] = True
+        return app
 
     def setUp(self):
-        self.db_fd, aggtron.app.config['MONGODB_SETTINGS'] = {'DB': 'testing'}
-        aggtron.app.config['TESTING'] = True
-        self.app = aggtron.app.test_client()
-        aggtron.init_db()
+        db.create_all()
 
     def tearDown(self):
-        os.close(self.db_fb)
-        os.unlink(aggtron.app.config['MONGODB_SETTINGS'])
+        db.session.remove()
+        db.drop_all()
+
+    def test_create_user(self):
+        user = Users(email='eric1@eric1.com', password='eric')
+        db.session.add(user)
+        db.session.commit()
+
+        u = Users.query.filter_by(email='eric1@eric1.com').first()
+
+        print u.email
+        self.assertEqual('eric1@eric1.com', u.email) 
 
 
 if __name__ == '__main__':
