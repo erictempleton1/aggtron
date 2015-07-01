16430df2d9901b7a6f95f05cace32c24432fe90e
Eric Templeton <erictempleton1@gmail.com>
2015-07-01 06:39:55 -0400

    added new migrations folders, and proj listing on main site

--- main_site.py
+++ main_site.py
@@ -1,5 +1,7 @@
 import os, datetime
+from models import Users, Project
 from flask import Blueprint, render_template, url_for
+from flask.ext.login import current_user
 from jinja2 import TemplateNotFound
 
 
@@ -9,6 +11,11 @@ auth_pages = Blueprint('auth_pages', __name__)
 
 @main_site_index.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('main/index.html')
+    # list projects or no projects for user
+    if current_user.is_authenticated():
+        projects = Project.query.filter_by(created_by=current_user.id)
+    else:
+        projects = 'No projects created'    
+    return render_template('main/index.html', projects=projects)
 
 
