04f7677f95f661f607a0aa885a6bb2a49db4505b
Eric Templeton <erictempleton1@gmail.com>
2015-07-10 21:06:40 -0400

    fixed issue with none type returned from query

--- app/twitter/build_query.py
+++ app/twitter/build_query.py
@@ -8,30 +8,39 @@ from flask import Blueprint, render_template, request, flash, redirect
 
 build_query = Blueprint('build_query', __name__, template_folder='templates')
 
-# TODO pass arguments to url
+
 @build_query.route('/<int:pid>/queries')
 @login_required
 def main(pid):
+
     project = Project.query.filter_by(id=pid, created_by=current_user.id).first_or_404()
     proj_auth = AuthInfo.query.filter_by(project_name=project.id).first()
 
-    consumer_key = config.TWITTER_CONSUMER_KEY
-    consumer_secret = config.TWITTER_CONSUMER_SECRET
-    oauth_token = proj_auth.oauth_token
-    oauth_token_secret = proj_auth.oauth_token_secret
-    url = u'https://api.twitter.com/1.1/statuses/user_timeline.json'
+    if proj_auth:
+      # check to make sure auth creds are in the db
+      consumer_key = config.TWITTER_CONSUMER_KEY
+      consumer_secret = config.TWITTER_CONSUMER_SECRET
+      oauth_token = proj_auth.oauth_token
+      oauth_token_secret = proj_auth.oauth_token_secret
+      url = u'https://api.twitter.com/1.1/statuses/user_timeline.json'
 
-    query = OAuth1(
-                   consumer_key,
-                   consumer_secret,
-                   oauth_token,
-                   oauth_token_secret,
-                )
+      query = OAuth1(
+                     consumer_key,
+                     consumer_secret,
+                     oauth_token,
+                     oauth_token_secret,
+                  )
+    else:
+        # prompt user to authenticate if no auth creds are found
+        proj_auth = False
+
+
     try:
+        # make a basic query to confirm connection
         r = requests.get(url=url, auth=query)
-        basic_query = r.json()[1]['text']
+        basic_query = r.json()[0]['text']
     except:
-        basic_query = 'An error has occured'    
+        basic_query = 'Error: http status code: {0}'.format(r.status_code)    
 
     return render_template(
                            'main/build_query.html',
