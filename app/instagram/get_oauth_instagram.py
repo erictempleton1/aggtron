d1973b0c20a136445aae878e659a9488d19ff8f1
Eric Templeton <erictempleton1@gmail.com>
2015-07-28 20:25:38 -0400

    working on oauth redirect

--- app/instagram/get_oauth_instagram.py
+++ app/instagram/get_oauth_instagram.py
@@ -14,31 +14,26 @@ get_oauth_instagram = Blueprint('get_oauth_instagram', __name__, template_folder
 
 instagram_client_id = config.INSTAGRAM_CLIENT_ID
 instagram_client_secret = config.INSTAGRAM_CLIENT_SECRET
+redirect_uri = 'http://localhost:5000/instagram/oauth/callback'
 
 
 @get_oauth_instagram.route('/<int:pid>/instagram/oauth', methods=['GET'])
 @login_required
 def request_token(pid):
 
-    redirect_uri = 'http://localhost:5000/instagram/oauth/callback?pid={0}'.format(pid)
-    base_url = 'https://api.instagram.com/oauth/authorize/'
+    authorize_url = 'https://api.instagram.com/oauth/authorize/'
 
-    params = {
-                'client_id': instagram_client_id,
-                'redirect_uri': redirect_uri,
-                'response_type': 'code'
-            }
-    insta_url = requests.get(base_url, params)          
+    oauth = OAuth2Session(client_id=instagram_client_id, redirect_uri=redirect_uri)
 
-    return redirect(insta_url.url)
+    return redirect(oauth.authorization_url(authorize_url))
 
 # example response url -
 # http://localhost:5000/instagram/oauth/callback?pid=1&code=1b5c96d1fdac407290a3d3eb11f56dea
 
 # not working correctly
-@get_oauth_instagram.route('/instagram/oauth/callback?pid=<int:pid>&code=<code>', methods=['GET'])
+@get_oauth_instagram.route('/instagram/oauth/callback', methods=['GET'])
 @login_required
-def callback(pid, code):
+def callback():
     
     redirect_uri = 'http://localhost:5000/instagram/oauth/callback?pid={0}'.format(pid)
     request_token_uri = 'https://api.instagram.com/oauth/access_token'
