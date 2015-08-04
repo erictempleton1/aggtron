ea29cdccf90520c9ca94c47bbb5d5ba2a65c754d
Eric Templeton <erictempleton1@gmail.com>
2015-08-03 21:30:44 -0400

    working on fixing tests...

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 import unittest
-from app import app, db
-from flask.ext.login import login_user
+from flask import Flask, current_app
+from app import app, db, flask_bcrypt
+from models import Users
+from flask.ext.login import login_user, current_user
 from flask.ext.testing import TestCase
 
 
@@ -9,28 +11,31 @@ class AggtronTestCase(unittest.TestCase):
 
 
     def setUp(self):
+        app.config['TESTING'] = True
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/erictempleton/Documents/Projects/myenv/aggtron_test.db'
-        self.client = app.test_client()
+        self.app = app.test_client()
         db.create_all()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
 
-    def test_basic(self):
+    def test_basic_load(self):
         """ test basic page loads """
-        rv = self.client.get('/')
+        rv = self.app.get('/')
         assert 'Aggtron' in rv.data
 
-        rv = self.client.get('/login')
+        rv = self.app.get('/login')
         assert 'Login' in rv.data
 
-        rv = self.client.get('/register')
+        rv = self.app.get('/register')
         assert 'Register' in rv.data
 
-        rv = self.client.post('/login', data=dict(email='eric@eric.com', password='eric'))
-        print rv.data
-        assert 'eric@eric.com' in rv.data  
+    def login(self, email, password):
+        return self.app.post('/login', data=dict(
+            email=email,
+            password=password
+            ), follow_redirects=True)
 
 
 if __name__ == '__main__':
