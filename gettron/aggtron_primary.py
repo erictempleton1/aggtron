173ce2c7368b416a15b3adda912aa8a2606e1124
Eric Templeton <erictempleton1@gmail.com>
2015-08-25 21:05:53 -0400

    finishing back sqlalchemy query

--- gettron/aggtron_primary.py
+++ gettron/aggtron_primary.py
@@ -1,3 +1,26 @@
-from sqlalchemy import create_engine
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('/home/erictempleton/Documents/Projects/myenv/aggtron/aggtron.db', echo=True)
+# create engine and get metadata
+Base = declarative_base()
+engine = create_engine('sqlite:////home/erictempleton/Documents/Projects/myenv/aggtron/aggtron.db', echo=True)
+metadata = MetaData(bind=engine)
+
+# set session config
+Session = sessionmaker()
+Session.configure(bind=engine)
+
+# reflect the table we need to use
+class Project(Base):
+    __table__ = Table('project', metadata, autoload=True)
+
+# create the session to use declared tables
+session = Session()
+
+# basic query to get all of the data from the declared table
+project_query = session.query(Project).all()
+
+# list project names from above query
+for x in project_query:
+    print x.name
