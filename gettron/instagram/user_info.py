89f559e127b1801d96f13405b830b765174cb97d
Eric Templeton <erictempleton1@gmail.com>
2015-09-03 20:26:07 -0400

    adding authinfo and checking query

--- gettron/instagram/user_info.py
+++ gettron/instagram/user_info.py
@@ -1,20 +1,29 @@
-from sqlalchemy import create_engine
+
+from sqlalchemy import create_engine, Table, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # create engine and get metadata
 Base = declarative_base()
-engine = create_engine('sqlite:////home/erictempleton/Documents/Projects/myenv/aggtron/aggtron.db', echo=True)
+engine = create_engine('sqlite:////home/erictempleton/Documents/Projects/myenv/aggtron/aggtron.db', echo=False)
+metadata = MetaData(bind=engine)
 
 # set session config
 Session = sessionmaker()
 Session.configure(bind=engine)
 
-# reflect user info table
+
+# reflect existing tables
 class UserInfo(Base):
     __table__ = Table('instagramuserinfoquery', metadata, autoload=True)
 
 
+class AuthInfo(Base):
+    __table__ = Table('authinfo', metadata, autoload=True)
+
 
 # create the session to use declared tables
 session = Session()
+
+for x in session.query(UserInfo):
+    print x.name
