d6934b17f54f44e554d9befb4d652c0bf12e0bec
Eric Templeton <erictempleton1@gmail.com>
2015-09-23 08:16:27 -0400

    adjusting tests and adding timeline files

---
+++ gettron/instagram/instagram_timeline_tests.py
@@ -0,0 +1,21 @@
+import requests
+import unittest, datetime
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Table, MetaData
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+
+import sys
+sys.path.insert(0, '../..')
+import config
+
+
+def basic_query():
+    params = {'access_token': config.INSTAGRAM_TEST_TOKEN}
+    timeline_url = 'https://api.instagram.com/v1/users/self/media/recent/'
+    r = requests.get(timeline_url, params)
+    return r.json()
+
+class TestUserTimeline(unittest.TestCase):
+    pass
