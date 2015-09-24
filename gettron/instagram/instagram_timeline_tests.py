d3498c99217c46719088c19eaefdd846e7ad69f3
Eric Templeton <erictempleton1@gmail.com>
2015-09-24 08:07:02 -0400

    adding to timeline model

--- gettron/instagram/instagram_timeline_tests.py
+++ gettron/instagram/instagram_timeline_tests.py
@@ -12,7 +12,11 @@ import config
 
 
 def basic_query():
-    params = {'access_token': config.INSTAGRAM_TEST_TOKEN}
+    params = {
+                'access_token': config.INSTAGRAM_TEST_TOKEN,
+                'count': 1
+            }
+            
     timeline_url = 'https://api.instagram.com/v1/users/self/media/recent/'
     r = requests.get(timeline_url, params)
     return r.json()
---
+++ gettron/instagram/user_timeline_model.py
@@ -0,0 +1,31 @@
+import datetime
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+
+
+Base = declarative_base()
+engine = create_engine('sqlite:////home/erictempleton/Documents/Projects/myenv/aggtron/aggtron.db', echo=True)
+
+
+class AggInstagramUserTimeline(Base):
+
+    __tablename__ = 'agginstagramusertimeline'
+
+    id = Column(Integer, primary_key=True)
+
+    # project and query id from aggtron
+    query_id = Column(Integer)
+
+    # data from instagram API query response
+    comment_count = Column(Integer)
+    created_time = Column(Integer)
+    img_filter = Column(String)
+    img_thumb_url = Column(String)
+    img_std_url = Column(String)
+
+
+    def __repr__(self):
+        return '<Caption: {0}>'.format(self.caption)
+
