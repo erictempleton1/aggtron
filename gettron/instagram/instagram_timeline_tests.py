dc01e97f181e4b687ec2edcc101f14401da9d57b
Eric Templeton <erictempleton1@gmail.com>
2015-10-14 09:39:38 -0500

    removing extra code from tests

--- gettron/instagram/instagram_timeline_tests.py
+++ gettron/instagram/instagram_timeline_tests.py
@@ -16,16 +16,6 @@ engine = create_engine('sqlite:////home/erictempleton/Documents/Projects/myenv/a
 Base = declarative_base()
 
 
-def basic_query():
-    params = {
-                'access_token': config.INSTAGRAM_TEST_TOKEN,
-                'count': 1
-            }
-
-    timeline_url = 'https://api.instagram.com/v1/users/self/media/recent/'
-    r = requests.get(timeline_url, params)
-    return r.json()
-
 class TestUserTimeline(unittest.TestCase):
     
     def setUp(self):
