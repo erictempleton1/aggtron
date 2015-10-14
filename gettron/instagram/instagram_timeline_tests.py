b0ee0c71d74f0a7a8cffdae5025f71cb7f7a8897
Eric Templeton <erictempleton1@gmail.com>
2015-10-14 10:02:00 -0500

    adding basic request test

--- gettron/instagram/instagram_timeline_tests.py
+++ gettron/instagram/instagram_timeline_tests.py
@@ -6,6 +6,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, Table, MetaData
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 
+from user_timeline import GetTimelineInfo
+
 import sys
 sys.path.insert(0, '../..')
 import config
@@ -29,9 +31,30 @@ class TestUserTimeline(unittest.TestCase):
         # bind an individual session to connection
         self.session = Session(bind=self.connection)
 
+        self.insta = GetTimelineInfo()
+        self.timeline_url = 'https://api.instagram.com/v1/users/self/media/recent/'
+        self.access_token = config.INSTAGRAM_TEST_TOKEN
+
+
         # create db and tables
         # ignored by default if db and table already exists
         Base.metadata.create_all(engine)
+
+    def test_request(self):
+        """
+        test baseline request to the instagram API
+        """
+        base_req = self.insta.base_request(
+                                           self.access_token,
+                                           self.timeline_url
+                                        )
+
+        # test that the request can be made
+        self.assertTrue(base_req)
+
+        # test that the response contains actual user data
+        self.assertTrue(u'user' in base_req['data'][0])
+        
 
     def tearDown(self):
         self.session.close()
