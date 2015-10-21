9bcd0aa2cd8eaf6234fcfa0b890244ce44192cfe
Eric Templeton <erictempleton1@gmail.com>
2015-10-21 09:52:14 -0700

    initial user info test layout

--- gettron/twitter/tests/user_info_tests.py
+++ gettron/twitter/tests/user_info_tests.py
@@ -10,3 +10,41 @@ from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 import sys
 sys.path.insert(0, '../../..')
 import config
+
+
+# global app scope
+Session = sessionmaker()
+engine = create_engine('sqlite:////home/erictempleton/Documents/Projects/myenv/aggtron/aggtron_test.db', echo=True)
+Base = declarative_base()
+
+
+class TestTwitterQuery(unittest.TestCase):
+
+    def setUp(self):
+
+        # connect to db
+        self.connection = engine.connect()
+
+        # begin a non-ORM transaction
+        self.trans = self.connection.begin()
+
+        # bind an individual Session to the connection
+        self.session = Session(bind=self.connection)
+
+        # create db and tables
+        # ignored by default if db and table already exists
+        Base.metadata.create_all(engine)
+
+    def tearDown(self):
+        self.session.close()
+
+        # rollback everything from above
+        self.trans.rollback()
+
+        # return the connection to the engine
+        self.connection.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
+                
