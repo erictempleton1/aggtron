5598f55641016ee78a60be9ce822f28f54bfbf9c
Eric Templeton <erictempleton1@gmail.com>
2015-10-22 10:09:29 -0700

    added test table and basic request

--- gettron/twitter/tests/user_info_tests.py
+++ gettron/twitter/tests/user_info_tests.py
@@ -10,12 +10,36 @@ from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 import sys
 sys.path.insert(0, '../../..')
 import config
+sys.path.insert(0, '../')
+from user_info import GetUserInfo
 
 
 # global app scope
 Session = sessionmaker()
 engine = create_engine('sqlite:////home/erictempleton/Documents/Projects/myenv/aggtron/aggtron_test.db', echo=True)
 Base = declarative_base()
+
+
+class AggTwitterUserInfo(Base):
+    """ re-create user info table for testing """
+
+    __tablename__ = 'aggtwitteruserinfo'
+
+    id = Column(Integer, primary_key=True)
+
+    # project query id from aggtron
+    query_id = Column(Integer)
+    username = Column(String)
+    user_id = Column(Integer)
+    favorites_count = Column(Integer)
+    listed_count = Column(Integer)
+    followers_count = Column(Integer)
+    statuses_count = Column(Integer)
+    friends_count = Column(Integer)
+    date = Column(DateTime, default=datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return '<Username: {0}>'.format(self.username)
 
 
 class TestTwitterQuery(unittest.TestCase):
@@ -35,6 +59,13 @@ class TestTwitterQuery(unittest.TestCase):
         # ignored by default if db and table already exists
         Base.metadata.create_all(engine)
 
+    def test_base_request(self):
+        """ test basic request to the twitter API """
+        info = GetUserInfo()
+        auth_token = config.TWITTER_TEST_TOKEN
+        base_req = info.base_request(auth_token)
+        print base_req    
+
     def tearDown(self):
         self.session.close()
 
@@ -47,4 +78,3 @@ class TestTwitterQuery(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-                
