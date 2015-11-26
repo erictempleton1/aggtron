6ade2a9d20b63c99bed2cf83bd8794c8cd5ac9b5
Eric Templeton <erictempleton1@gmail.com>
2015-11-25 20:56:54 -0500

    adding main query for user timeline

--- gettron/twitter/user_timeline.py
+++ gettron/twitter/user_timeline.py
@@ -36,7 +36,9 @@ session = Session()
 
 class GetUserTimeline(object):
 
-    def __init__(self):
+    def __init__(self, access_key, access_secret):
+        self.access_key = access_key
+        self.access_secret = access_secret
         self.queries = session.query(UserTimeline)
         self.timeline_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
 
@@ -45,8 +47,8 @@ class GetUserTimeline(object):
         oauth_params = OAuth1(
                               config.TWITTER_CONSUMER_KEY,
                               config.TWITTER_CONSUMER_SECRET,
-                              access_key,
-                              access_secret
+                              self.access_key,
+                              self.access_secret
                             )
 
         params = {
@@ -60,7 +62,45 @@ class GetUserTimeline(object):
             # if the id does not exist for some reason
             json_result = False
             print e
-        yield json_result 
+        yield json_result
+
+    def get_timeline(self):
+        """
+        Make requests to get as much of the user's timeline as possible
+        """
+        # make initial request for first 200 results
+        make_req = self.base_request.next()
+
+        for res in make_req:
+            yield res
+
+        # get the last if from the list
+        max_id = make_req[-1]['id']
+
+        # subtract one from max_id and use this to query next 200
+        next_max = max_id - 1
+
+        while max_id:
+
+            # make the query again using next_max
+            base_req = self.base_request(
+                self.access_key,
+                self.access_secret,
+                max_id=next_max
+            )
+
+            make_req = base_req.next()
+
+            for res in make_req:
+                yield res
+
+            try:
+                # keep trying to get the max_id until some limit is hit, then break out of the loop
+                # could be a quota limit, 3200 tweet limit, or we got all tweets
+                max_id = make_req[-1]['id']
+                next_max = max_id - 1
+            except Exception:
+                max_id = False
         
     def get_tokens(self, auth_id):
         """ query for access token from auth info table """
