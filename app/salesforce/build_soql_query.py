0faaa251cb5feddcb5f70ff52d050babae3d4b20
Eric Templeton <erictempleton1@gmail.com>
2016-01-14 07:00:12 -0500

    adding blueprint routes

--- app/salesforce/build_soql_query.py
+++ app/salesforce/build_soql_query.py
@@ -2,4 +2,7 @@ import config
 import requests
 from app import db
 from flask.ext.login import current_user, login_required
-# from forms import Salesforce
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+
+
+build_soql = Blueprint('build_soql', __name__, template_folder='templates')
